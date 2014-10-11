d1ac68bfe42b49ada64cf398e95b16f401b32dff
Adam Carter <adamrcarter@gmail.com>
2014-10-11 14:15:27 -0500

    added a code comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-   System.out.println("What even is this");
+    System.out.println("What even is this");
 	}
 }
+// wow this takes more work than Ruby....
