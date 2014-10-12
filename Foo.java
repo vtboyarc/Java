5fc30f307f545c09ed67672d31991dd407acbd75
Adam Carter <adamrcarter@gmail.com>
2014-10-12 18:35:28 -0500

    practice with Java loops

---
+++ Foo.java
@@ -0,0 +1,13 @@
+class Foo {
+  public static void main(String [] args) {
+    
+    int x = 5;
+    
+    while (x > 1) {
+      x = x - 1;
+      if (x < 3) {
+        System.out.println("This is a small number!");
+      } 
+    }
+  }
+}
