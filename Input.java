960491c75c0a8613ff1482d398d032582fc9def4
Adam Carter <adamrcarter@gmail.com>
2014-10-12 18:45:27 -0500

    wip on getting user input w/Java

--- Input.java
+++ Input.java
@@ -10,15 +10,15 @@ class Input
  
       Scanner in = new Scanner(System.in);
  
-      System.out.println("Enter a string");
+      System.out.println("Enter a string!");
       s = in.nextLine();
       
  
-      System.out.println("Enter an integer");
+      System.out.println("Enter an integer!");
       a = in.nextInt();
      
  
-      System.out.println("Enter a float");
+      System.out.println("Enter a float(or don't, but I'll turn it into a float)!");
       b = in.nextFloat();
       System.out.println("You entered string "+s);
       System.out.println("You entered integer "+a);
